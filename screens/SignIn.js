151a5b9ab4a0e5c9534070e1bb5bcea62896a510
ikkei12 <ikkei12.h@gmail.com>
2020-11-04 21:01:37 +0900

    add Sign In

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, TextInput } from "react-native";
 import User from "../User";
+import axios from "../plugins/axios.js";
+import ENV from "../environments";
 
 export default class SignIn extends Component {
-  onPressButton() {
-    User.login();
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+  async onPressButton() {
+    // User.login();
+    await axios
+      .post("/api/v1/users/signup", { name: this.state.name })
+      .then((res) => console.log(res))
+      .catch((e) => {
+        console.log(e);
+      });
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text>sign In</Text>
+        <Text>Sign In</Text>
+        <TextInput
+          onChangeText={(name) => this.setState({ name })}
+          value={this.state.name}
+        />
         <Button
           onPress={this.onPressButton}
-          title="Learn More"
+          title="Sign In"
           color="#841584"
-          accessibilityLabel="Learn more about this purple button"
+          accessibilityLabel="Sign In about button"
         />
       </View>
     );
